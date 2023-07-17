e825a6824357992f1d5f01614f672d272f1df10e
chl <987821630@qq.com>
2023-07-17 10:29:18 +0800

    string

--- test.sv
+++ test.sv
@@ -3,5 +3,9 @@ string str1 ="Hello RKV" ;
 initial begin
 foreach(str1[i])
 $display ( "str1 [%0d]=%s", i, str1[i]);
+str1[0]="y";
+$display("str1 = %s",str1);        //str1[0] is 'y'
+str1[0]="hello";
+$display("str1 = %s",str1);        //str1[0] is 'o'
 end
 endmodule
